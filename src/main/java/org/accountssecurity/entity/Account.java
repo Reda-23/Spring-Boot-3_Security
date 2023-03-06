d633dc8cee9ef9c48f71f3553054a7d87e4bf438
Reda-23 <readhajjaj@gmail.com>
2023-03-06 16:17:51 +0100

    Creating The Entity Class 'Account'

---
+++ src/main/java/org/accountssecurity/entity/Account.java
@@ -0,0 +1,83 @@
+package org.accountssecurity.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+
+@Entity
+public class Account {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String role;
+    private Date date;
+    private String fullName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Account() {
+    }
+
+    public Account(Long id, String username, String password, String role, Date date, String fullName) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.date = date;
+        this.fullName = fullName;
+    }
+}
