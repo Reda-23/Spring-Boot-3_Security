60196abc5404376b40631a6a0eee306ab1d108b6
Reda-23 <readhajjaj@gmail.com>
2023-03-06 16:13:24 +0100

    Initial commit

---
+++ src/main/java/org/accountssecurity/AccountsSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.accountssecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/accountssecurity/AccountsSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.accountssecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
